a87f31ab7be419dea951cc1e05ff7814553c6eac
Yi Xu <alanxu1220@gmail.com>
2021-03-15 15:06:17 +0800

    1. Updated UI for profile page and streaming page 2. Updated Realm settings 3. Implemented search functionality in Stream page. User can now search for zoom rooms to join

--- KeepFit/UserProfile.swift
+++ KeepFit/UserProfile.swift
@@ -10,11 +10,24 @@ import RealmSwift
 import Realm
 
 class UserProfile: Object {
+    convenience init(account: String = "", nickname: String = "", birthday: String = "", height: String = "", weight: String = "", profilePhotoURL: String = "", zoomLink: String = "") {
+        self.init()
+        self.account = account
+        self.nickname = nickname
+        self.birthday = birthday
+        self.height = height
+        self.weight = weight
+        self.profilePhotoURL = profilePhotoURL
+        self.zoomLink = zoomLink
+    }
+    
     @objc dynamic public var account: String = ""
     @objc dynamic public var nickname: String = ""
     @objc dynamic public var birthday: String = ""
     @objc dynamic public var height: String = ""
     @objc dynamic public var weight: String = ""
     @objc dynamic public var profilePhotoURL: String = ""
+    @objc dynamic public var zoomLink: String = ""
+    
     
 }
--- KeepFit/UserProfileModel.swift
+++ KeepFit/UserProfileModel.swift
@@ -14,8 +14,52 @@ class UserProfileModel: NSObject {
     
     static let shared = UserProfileModel()
     
+    // Initiate MongoDB Realm
+    let realm = RealmModel.shared.synchronizedRealm
     
     override init() {
-    
+        // For testing
+//        var testing_profile = UserProfile(account: "User", nickname: "R Pattis", birthday: "12/12", height: "100", weight: "50", profilePhotoURL: "...", zoomLink: "ZoomLink")
+//
+//        userProfiles.append(testing_profile)
+        
+        super.init()
+        render()
+        
     }
+    
+    func searchForZoomRooms(search_text: String) -> Array<UserProfile> {
+        var result = Array<UserProfile>()
+        
+        
+        if search_text == "" {
+            return userProfiles
+        }
+        
+        //Cite: https://www.hackingwithswift.com/articles/108/how-to-use-regular-expressions-in-swift
+        let range = NSRange(location: 0, length: search_text.utf16.count)
+        let regex = try! NSRegularExpression(pattern: "\(search_text)")
+        for targetUserProfile in userProfiles {
+            if regex.firstMatch(in: targetUserProfile.nickname, options: [], range: range) != nil {
+                result.append(targetUserProfile)
+            }
+        
+        }
+        return result
+    }
+    
+    func numberOfExistingUsers() -> Int {
+        return userProfiles.count
+    }
+    
+    func render() {
+        let saved_profiles = realm.objects(UserProfile.self)
+        
+        userProfiles = Array<UserProfile>()
+        for saved in saved_profiles {
+            userProfiles.append(saved)
+        }
+        
+    }
+    
 }
