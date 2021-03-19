f34951fd845128ec69d1be88a773cd7cc6ef3a33
Yi Xu <alanxu1220@gmail.com>
2021-03-19 14:04:15 +0800

    Minor fixes

--- KeepFit/RealmModel.swift
+++ KeepFit/RealmModel.swift
@@ -24,32 +24,7 @@ class RealmModel {
         return land
 }()
     
-    public var synchronizedRealm: Realm? = nil // = {
-//        () -> Realm in
-//
-//        let login = app.login(credentials: Credentials.emailPassword(email: "test@test.com", password: "testtest"))  { (result) in
-//            switch result {
-//            case .failure(let error):
-//                print("Login failed: \(error)")
-//            case .success(let user):
-//                print("Login as \(user) succeeded!")
-//            }
-//        }
-//
-//        let user = app.currentUser!
-//        let partitionValue = "keepfitpartition"
-//        var configuration = user.configuration(partitionValue: partitionValue)
-//
-//
-//        configuration.fileURL = configuration.fileURL!.deletingLastPathComponent().appendingPathComponent("synchronizedRealm.realm")
-//        configuration.objectTypes = [UserProfile.self]
-//        let land = try! Realm(configuration: configuration)
-//        print("Opened realm: \(land)")
-//
-//
-//
-//        return land
-//}()
+    public var synchronizedRealm: Realm? = nil 
     
     public func connectToRemoteRealm() {
         let user = app.currentUser!
@@ -70,8 +45,5 @@ class RealmModel {
     
 }
 
-// private api key: 32f65951-8c04-44a0-b515-a6a699bb4b2b
-// public api key: BUXJHHXJ
-// real-app id: tasktracker-xwbdh
 
 
