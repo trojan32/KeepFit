a3596f84e36cbcea6a8c08902215ed89c55b86db
Zhenyi Yang <zhenyiya@usc.edu>
2021-03-25 00:06:55 -0700

    profile

--- KeepFit/ProfilePageViewController.swift
+++ KeepFit/ProfilePageViewController.swift
@@ -6,53 +6,63 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class ProfilePageViewController: UIViewController {
     
     let personalAccountModel = PersonalAccountModel.shared
 
-    
     @IBOutlet var profileImage: UIImageView!
-    
     @IBOutlet var nicknameLabel: UILabel!
     @IBOutlet var birthdayLabel: UILabel!
-    
-    
     @IBOutlet var heightLabel: UILabel!
     @IBOutlet var weightLabel: UILabel!
     
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        if !personalAccountModel.loggedIn {
-            performSegue(withIdentifier: "loginSegue", sender: self)
-        } else {
+        
+        if Auth.auth().currentUser != nil {
+          // User is signed in.
             print("user logged in")
             loadAccountInfo()
+        } else {
+          // No user is signed in.
+            performSegue(withIdentifier: "loginSegue", sender: self)
         }
+//        if !personalAccountModel.loggedIn {
+//            performSegue(withIdentifier: "loginSegue", sender: self)
+//        } else {
+//            print("user logged in")
+//            loadAccountInfo()
+//        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     func loadAccountInfo() {
-        
         // Cite: https://github.com/nealight/Apply
-        
         print("loading account info")
         
+        let db = Firestore.firestore()
+        let currentUser = Auth.auth().currentUser
+        let currentUID = currentUser?.uid as! String
+        let usersRef = db.collection("users")
+        usersRef.child(currentUID).observeSingleEvent(of: .value, with: { (snapshot) in
+        let userSnapshot = snapshot.value as? NSDictionary
+        let nickname = userSnapshot?["nickname"] as? String ?? ""
+        let birthday = userSnapshot?["birthday"] as? String ?? ""
+        let height = userSnapshot?["height"] as? String ?? ""
+        let weight = userSnapshot?["weight"] as? String ?? ""
+        }) { (error) in print(error.localizedDescription) }
+        
         loadProfileImage()
-        nicknameLabel.text = personalAccountModel.personalAccount.nickname
-        birthdayLabel.text = "Birthday: \( personalAccountModel.personalAccount.birthday)"
-        heightLabel.text = "Height: \(personalAccountModel.personalAccount.height) cm"
-        weightLabel.text = "Weight: \(personalAccountModel.personalAccount.weight) kg"
-        
-
-        
-        
+        nicknameLabel.text = nickname
+        birthdayLabel.text = "Birthday: \(birthday)"
+        heightLabel.text = "Height: \(height) cm"
+        weightLabel.text = "Weight: \(weight) kg"
         return
     }
     
@@ -60,35 +70,24 @@ class ProfilePageViewController: UIViewController {
         let imageName = personalAccountModel.personalAccount.profilePhotoURL
         
         if imageName != "Default" {
-            
             let manager = FileManager.default
             let url = manager.urls(for: .documentDirectory, in: .userDomainMask).first
-            
-            
             let imagePath = url!.appendingPathComponent(imageName)
-            
             print(imagePath)
-            
             do {
                 profileImage.image = UIImage(data: try Data(contentsOf: imagePath))
-                
             } catch {}
         }
     }
     
     
-    @IBAction func logOut(_ sender: UIButton) {
-        let _ = personalAccountModel.logOutOfAccount()
+    @IBAction func logOut(_ sender: UIButton)
+    {
+//        let _ = personalAccountModel.logOutOfAccount()
+      do{
+        try Auth.auth().signOut()
+      } catch let error {
+        print(error)
+      }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
