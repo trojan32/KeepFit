31ded56bcbe39307acd3733d02e8fc9583ddd715
Zhenyi Yang <zhenyiya@usc.edu>
2021-03-25 01:01:57 -0700

    Profile edit

---
+++ KeepFit/ProfileEditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileEditViewController.swift
+//  KeepFit
+//
+//  Created by Albert on 2021/3/25.
+//
+
+import UIKit
+
+class ProfileEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
