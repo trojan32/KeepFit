07a77458067e355a81816f38fa740fb8cb1d129c
Zhenyi Yang <zhenyiya@usc.edu>
2021-03-29 10:55:15 -0700

    Fixed an IMPOSSIBLE BUG
    I don't remember how many times I tell u guys that I can't run Keepfit in my Mac Xcode simulator even after I fixed my internet issue completely. Yi says it's probably because my Mac is too old and getting slow. WRONG!!! I fixed that bug. It's because the Workout View Controller that Kevin create have some kind of bugs. Because that's the only exception caught in my console. This is the error in case u want to know: "'NSUnknownKeyException', reason: '[<UIViewController 0x7fa425415b00> setValue:forUndefinedKey:]: this class is not key value coding-compliant for the key workoutTableView.'" Moreover, there is a very interesting thing i found: when open View->Main.storyboard, if u r in the assistant editor automatic mode, if u click one view controller, it should show u the corresponding .swift automatically as long as u set it up correctly. Well, if i click any view controller other then the WorkoutNowViewController, their corresponding .swift will show up, meaning it's the problem of the file rather than the Xcode assistant editor automatic mode. The way I fix it is: copy all the code from the original WorkoutNowViewController, rename the controller to WorkoutNowViewController_tmp, create a new UIViewController and name it WorkoutNowViewController, paste the code from the original file. Here is the MAGIC, it worked!!!
    I think probably, when Kevin create this file, there r mistakes introduced, such as wrong selection of the type of the controller file (UIView vs UIViewController vs UITableViewController), or the target setting when create the file are wrong.

---
+++ KeepFit/WorkoutNowViewController_tmp.swift
@@ -0,0 +1,73 @@
+//
+//  WorkoutNowViewController.swift
+//  KeepFit
+//
+//  Created by kevin on 3/14/21.
+//
+
+import UIKit
+
+class WorkoutNowViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var workoutTableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        
+
+        self.workoutTableView.delegate = self
+        self.workoutTableView.dataSource = self
+    }
+    
+    let workoutItems = ["Yoga","Swimming","Running","Cycling","HITT","Zumba","Crossfit","Orange Theory'","Lifting","Ping Pong","Stretching"]
+
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return workoutItems.count
+    }
+    
+//    @IBOutlet weak var workoiut: UITableViewCell!
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "workoutCell", for: indexPath)
+
+        cell.textLabel?.text = workoutItems[indexPath.row]
+//        let imageName = UIImage(named: transportItems[indexPath.row])
+//        cell.imageView?.image = imageName
+
+        return cell
+    }
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
+////
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        return UITableViewCell()
+//    }
+//    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+//            // cell selected code here
+//        }
+    
+    
+
+
+  
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
