a6529d7e160d8e38e4afad3bf6c2fe371624f641
Gloria Liu <liuglori@usc.edu>
2021-03-30 20:33:06 +0800

    Update ExerciseDetailViewController.swift

--- KeepFit/ExerciseDetailViewController.swift
+++ KeepFit/ExerciseDetailViewController.swift
@@ -13,17 +13,65 @@ class ExerciseDetailViewController: UIViewController {
     @IBOutlet var startStopButton: UIButton!
     var started = false;
     
+    var timer = Timer()
+    var hour = 0
+    var minute = 0
+    var second = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        timerDisplay.text = convertToTimerDisplay(minute: 0, second: 0)
+        timerDisplay.text = convertToTimerDisplay(hour:0, minute: 0, second: 0)
 
         // Do any additional setup after loading the view.
+
     }
     
-    func convertToTimerDisplay(minute: Int, second: Int) -> String {
-        return "\(minute):\(second)"
+    func convertToTimerDisplay(hour: Int, minute: Int, second: Int) -> String {
+        return "\(hour):\(minute):\(second)"
     }
+    
+    
+    @objc func timerAction(){
+        if(second == 59)
+        {
+            second = 0
+            if(minute == 59)
+            {
+                hour += 1
+                minute = 0
+            }
+            else
+            {
+                minute += 1
+            }
+        }
+        else
+        {
+            second += 1
+        }
+        timerDisplay.text = convertToTimerDisplay(hour: hour, minute: minute, second: second)
+    }
+    
+    @IBAction func timerButtonTapped(_ sender: UIButton)
+    {
+        if(!started)
+        {
+            print("start timer")
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+            started = true
+        }
+        else
+        {
+            print("end timer")
+            timer.invalidate()
+            started = false
+        }
+        
+    }
+    
+    
+    
     
     
 
