f031f7aba7c587f9eaee8c0d827c98dd28731cbe
Zhenyi Yang <zhenyiya@usc.edu>
2021-05-10 07:37:34 -0700

    28. (3). The user cannot upload extremely large video files. You should set a restriction on the file size and video length.

--- KeepFit/UploadVideoViewController.swift
+++ KeepFit/UploadVideoViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import MobileCoreServices
 import AVKit
 import AVFoundation
+import CoreMedia
 import Firebase
 
 
@@ -16,9 +17,11 @@ class UploadVideoViewController: UIViewController, UIImagePickerControllerDelega
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        hideError()
     }
 
     @IBOutlet weak var videoTitle: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
     
     var picker = UIImagePickerController()
     var videoURL : NSURL?
@@ -36,7 +39,6 @@ class UploadVideoViewController: UIViewController, UIImagePickerControllerDelega
         if let videoUrl = info[UIImagePickerController.InfoKey.mediaURL] as? NSURL{
             print("Here is the file URL: \(videoUrl)")
             videoURL = videoUrl.filePathURL as? NSURL // this is the only thing I changed cause this is the file we are uploading
-//            videoURL = videoUrl
         }
         self.dismiss(animated: true, completion: nil)
     }
@@ -55,9 +57,26 @@ class UploadVideoViewController: UIViewController, UIImagePickerControllerDelega
     @IBAction func UploadTheVideo(_ sender: Any) {
         guard let videoURL = videoURL else { return }
         
+        // 28. (3). The user cannot upload extremely large video files. You should set a restriction on the file size and video length.
+        // file size should be less than 10M = 10240K = 10485760b
+        // video length should be less than 1min = 60s
+        let asset = AVURLAsset(url: videoURL as URL)
+        let videoSize = asset.fileSize
+        let durationInSeconds = asset.duration.seconds
+        print(videoSize ?? 0)
+        print(durationInSeconds)
+        
+        let error = validateFields(size: videoSize ?? 0, time: durationInSeconds)
+        if error != nil
+        {
+            showError(error!)
+            return
+        }
+        
+        // No Error
         let randomID = UUID.init().uuidString
         let storageRef = Storage.storage().reference(withPath: "Videos/\(randomID).mp4")
-        
+
         storageRef.putFile(from: videoURL as URL, metadata: nil) { (metaData, error) in
              // IMPORTANT: this is where I got the error from
             if error != nil {
@@ -92,6 +111,44 @@ class UploadVideoViewController: UIViewController, UIImagePickerControllerDelega
                 }
             }
         }
+        
+        showSuccessMessage()
+    }
+    
+    func validateFields(size: Int, time: Double) -> String? {
+        // Check that all fields are filled in
+        if size >= 10485760
+        {
+            return "Error: file size should be less than 10M (after compression)"
+        }
+        if time >= 60
+        {
+            return "Error: video length should be less than 1min"
+        }
+        return nil
     }
 
+    func showError(_ message:String) {
+        errorLabel.text = message
+        errorLabel.alpha = 1
+    }
+    
+    func hideError() {
+        errorLabel.alpha = 0
+    }
+    
+    func showSuccessMessage(){
+        errorLabel.text = "The video is uploaded successfully!"
+        errorLabel.alpha = 1
+        errorLabel.textColor = .green
+    }
+}
+
+extension AVURLAsset {
+    var fileSize: Int? {
+        let keys: Set<URLResourceKey> = [.totalFileSizeKey, .fileSizeKey]
+        let resourceValues = try? url.resourceValues(forKeys: keys)
+
+        return resourceValues?.fileSize ?? resourceValues?.totalFileSize
+    }
 }
